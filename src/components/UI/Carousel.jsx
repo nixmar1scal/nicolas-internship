6eeb52e4b2e5ec862af13d27e6fa2ef5163b1c5d
Nicolas <nicolas8mk@gmail.com>
2025-03-05 16:28:37 -0600

    fix

---
+++ src/components/UI/Carousel.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function Carousel({ items }) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="carousel-container">
+      <Slider {...settings}>
+        {items.map((item, index) => (
+          <div className="carousel-item" key={index}>
+            <div className="nft__item">
+              <div className="author_list_pp">
+                <Link
+                  to="/author"
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="top"
+                  title="Creator: Monica Lucas"
+                >
+                  <img className="author-image" src={item.authorImage} alt="" />
+                  <i className="fa fa-check"></i>
+                </Link>
+              </div>
+              <div className="de_countdown">{item.expiryDate}</div>
+
+              <div className="nft__item_wrap">
+                <div className="nft__item_extra">
+                  <div className="nft__item_buttons">
+                    <button>Buy Now</button>
+                    <div className="nft__item_share">
+                      <h4>Share</h4>
+                      <a href="" target="_blank" rel="noreferrer">
+                        <i className="fa fa-facebook fa-lg"></i>
+                      </a>
+                      <a href="" target="_blank" rel="noreferrer">
+                        <i className="fa fa-twitter fa-lg"></i>
+                      </a>
+                      <a href="">
+                        <i className="fa fa-envelope fa-lg"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
+                <Link to="/item-details">
+                  <img
+                    src={item.nftImage}
+                    className="nft__item_preview"
+                    alt=""
+                  />
+                </Link>
+              </div>
+              <div className="nft__item_info">
+                <Link to="/item-details">
+                  <h4>{item.title}</h4>
+                </Link>
+                <div className="nft__item_price">{item.price}</div>
+                <div className="nft__item_like">
+                  <i className="fa fa-heart"></i>
+                  <span>{item.likes}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
---
+++ src/components/UI/Countdown.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+
+function Countdown({ item }) {
+  const calculateTimeLeft = () => {
+    const now = Date.now();
+    const expiryTime = new Date(item.expiryDate).getTime();
+    const timeLeft = expiryTime - now;
+    return timeLeft > 0 ? timeLeft : 0;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newTimeLeft = calculateTimeLeft();
+      setTimeLeft(newTimeLeft);
+
+      if (newTimeLeft <= 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [item.expiryDate]);
+
+  const formatTime = (ms) => {
+    const seconds = Math.floor((ms / 1000) % 60);
+    const minutes = Math.floor((ms / 1000 / 60) % 60);
+    const hours = Math.floor((ms / 1000 / 60 / 60) % 24);
+    const days = Math.floor(ms / (1000 * 60 * 60 * 24));
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  };
+
+  return (
+    <div className="countdown">
+      <img src={item.nftImage} alt={item.title} />
+      <h3>{item.title}</h3>
+      <p>Price: {item.price} ETH</p>
+      <p>Likes: {item.likes} ❤</p>
+      <p>Expires in: {timeLeft > 0 ? formatTime(timeLeft) : "Expired"}</p>
+    </div>
+  );
+}
+
+export default Countdown;
