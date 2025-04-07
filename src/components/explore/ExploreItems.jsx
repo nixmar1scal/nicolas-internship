4b1acea10b4898a52ceea56dc6696639f5a5ce2f
Nicolas <nicolas8mk@gmail.com>
2025-04-07 16:46:13 -0500

    explore process

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,20 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import Countdown from "../UI/Countdown";
 
 const ExploreItems = () => {
+  const [nfts, setNfts] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(4);
+  const [sortOption, setSortOption] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchNFTs() {
+      try {
+        const { data } = await axios.get(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore`
+        );
+        setNfts(data);
+      } catch (err) {
+        console.error("Error fetching items", err);
+      }
+      setLoading(false);
+    }
+    fetchNFTs();
+  }, []);
+
+  useEffect(() => {
+    if (nfts.length === 0) return;
+
+    const sortedNfts = [...nfts].sort((a, b) => {
+      switch (sortOption) {
+        case "price_low_to_high":
+          return a.price - b.price;
+        case "price_high_to_low":
+          return b.price - a.price;
+        case "likes_high_to_low":
+          return b.likes - a.likes;
+        case "ending_soon":
+          return new Date(a.expiryDate) - new Date(b.expiryDate);
+        default:
+          return a.id - b.id;
+      }
+    });
+
+    setNfts(sortedNfts);
+  }, [sortOption, nfts.lenght]);
+
+  const handleFilterChange = (e) => {
+    setSortOption(e.target.value);
+  };
+
+  const loadMore = () => {
+    setVisibleCount((prev) => Math.min(prev + 4, nfts.length));
+  };
+
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select
+          id="filter-items"
+          className="form-select"
+          value={sortOption}
+          onChange={handleFilterChange}
+          disable={loading}
+        >
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
+          <option value="ending_soon">Ending Soon</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
+      {nfts.slice(0, visibleCount).map((item, index) => (
         <div
           key={index}
           className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
@@ -27,11 +83,11 @@ const ExploreItems = () => {
                 data-bs-toggle="tooltip"
                 data-bs-placement="top"
               >
-                <img className="lazy" src={AuthorImage} alt="" />
+                <img className="lazy" src={item.authorImage} alt="" />
                 <i className="fa fa-check"></i>
               </Link>
             </div>
-            <div className="de_countdown">5h 30m 32s</div>
+            <Countdown item={item} />
 
             <div className="nft__item_wrap">
               <div className="nft__item_extra">
@@ -51,28 +107,34 @@ const ExploreItems = () => {
                   </div>
                 </div>
               </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
+              <Link to={`/item-details${item.id}`}>
+                <img
+                  src={item.nftImage}
+                  className="lazy nft__item_preview"
+                  alt=""
+                />
               </Link>
             </div>
             <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
+              <Link to={`/item-details${item.id}`}>
+                <h4>{item.title}</h4>
               </Link>
-              <div className="nft__item_price">1.74 ETH</div>
+              <div className="nft__item_price">{item.price} ETH</div>
               <div className="nft__item_like">
                 <i className="fa fa-heart"></i>
-                <span>69</span>
+                <span>{item.likes}</span>
               </div>
             </div>
           </div>
         </div>
       ))}
-      <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
-      </div>
+      {!loading && visibleCount < nfts.lengh && (
+        <div className="col-md-12 text-center">
+          <button onClick={loadMore} className="btn-main lead">
+            Load more
+          </button>
+        </div>
+      )}
     </>
   );
 };
