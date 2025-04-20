f87eaab291a1cd78ebd9b07111ed283d47631d9a
Nicolas <nicolas8mk@gmail.com>
2025-04-19 23:46:29 -0500

    nico-auth-done

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
 import { Link, useParams } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
 import axios from "axios";
+import SkeletonAuthor from "../components/UI/SkeletonAuthor";
 
 const Author = () => {
   const { authorId } = useParams();
-  const [author, setAuthor] = useState([]);
+  const [author, setAuthor] = useState({});
   const [loading, setLoading] = useState(true);
+  const [isCopied, setIsCopied] = useState(false);
+  const [isFollowing, setIsFollowing] = useState(false);
 
   useEffect(() => {
     async function fetchAuthor() {
@@ -18,14 +20,40 @@ const Author = () => {
           `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`
         );
         setAuthor(data);
+
+        const storedFollow = localStorage.getItem(`followed-${authorId}`);
+        if (storedFollow === "true") {
+          setIsFollowing(true);
+        } else {
+          setIsFollowing(false);
+        }
       } catch (err) {
-        console.error("Error fetching author", err);
+        console.error("Error fetching author:", err);
       } finally {
         setLoading(false);
       }
     }
     fetchAuthor();
-  }, []);
+  }, [authorId]);
+
+  const toggleFollow = () => {
+    const updateFollow = !isFollowing;
+    setIsFollowing(updateFollow);
+    localStorage.setItem(`followed-${authorId}`, updateFollow);
+
+    setAuthor((prev) => ({
+      ...prev,
+      followers: prev.followers + (updateFollow ? 1 : -1),
+    }));
+  };
+
+  const copyWalletAddress = () => {
+    if (author?.address) {
+      navigator.clipboard.writeText(author.address);
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 2000);
+    }
+  };
 
   return (
     <div id="wrapper">
@@ -40,49 +68,62 @@ const Author = () => {
           style={{ background: `url(${AuthorBanner}) top` }}
         ></section>
 
-        <section aria-label="section">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+        {loading ? (
+          <SkeletonAuthor />
+        ) : (
+          <section aria-label="section">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="d_profile de-flex">
+                    <div className="de-flex-col">
+                      <div className="profile_avatar">
+                        <img src={author.authorImage} alt="" />
 
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4>
+                            {author.authorName}
+                            <span className="profile_username">
+                              @{author.tag}
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                              {author.address}
+                            </span>
+                            <button
+                              id="btn_copy"
+                              onClick={copyWalletAddress}
+                              title="Copy Text"
+                              className={isCopied ? "copied" : ""}
+                            >
+                              {isCopied ? "Copied!" : "Copy"}
+                            </button>
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          {author.followers} followers
+                        </div>
+                        <button className="btn-main" onClick={toggleFollow}>
+                          {isFollowing ? "Unfollow" : "Follow"}
+                        </button>
                       </div>
                     </div>
                   </div>
-                  <div className="profile_follow de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
-                    </div>
+                </div>
+
+                <div className="col-md-12">
+                  <div className="de_tab tab_simple">
+                    <AuthorItems author={author}/>
                   </div>
                 </div>
               </div>
-
-              <div className="col-md-12">
-                <div className="de_tab tab_simple">
-                  <AuthorItems />
-                </div>
-              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        )}
       </div>
     </div>
   );
